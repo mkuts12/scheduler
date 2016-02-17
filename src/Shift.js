90d23e3da992c5207276b7607f1177a4c1e7560a
Michael Kuts <mkuts12@gmail.com>
2016-02-17 19:07:44 +0200

    need an id

--- src/Shift.js
+++ src/Shift.js
@@ -3,9 +3,21 @@ import _ from 'lodash';
 export function newShift ({
     peopleNeeded,
     ptorim,
+    id,
 }, people) {
     return people.size > peopleNeeded ? undefined : {
         ptorim,
         people: _.sampleSize(people, peopleNeeded),
+    };
+}
+
+export function copyShift ({
+    ptorim,
+    id,
+}, newPeople) {
+    return {
+        ptorim,
+        id,
+        people: newPeople,
     };
 }
