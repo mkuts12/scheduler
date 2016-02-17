51e17b062d2a64d78ed7ab67aa2775a08e354a71
Michael Kuts <mkuts12@gmail.com>
2016-02-17 19:19:26 +0200

    need to save it somewhere

---
+++ src/options.js
@@ -0,0 +1,3 @@
+module.exports = {
+    "threshold": 100,
+}
--- src/stopCriteria.js
+++ src/stopCriteria.js
@@ -1,5 +1,5 @@
 import { threshold } from './options.json';
 
 export function stopCriterion (){
-    return this.statistics ? this.statistics.maxScore > threshold : 0;
+    return this.statistics ? this.statistics.maxScore > +( threshold ) : 0;
 }
