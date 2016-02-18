d66f6b39c52a500e23d4311d0e45aa9aad6be1e2
Michael Kuts <mkuts12@gmail.com>
2016-02-19 00:18:39 +0200

    draft of the first flugin. asks for specific solution

--- src/plugins/test.js
+++ src/plugins/test.js
@@ -2,4 +2,15 @@ import _ from 'lodash';
 
 export default function ( solution ) {
     let sum = 0;
+    sum += +( solution[0].people[ "1" ] );
+    sum += +( solution[0].people[ "3" ] );
+    sum += +( solution[0].people[ "5" ] );
+    sum += +( solution[1].people[ "1" ] );
+    sum += +( solution[1].people[ "2" ] );
+    sum += +( solution[1].people[ "3" ] );
+    sum += +( solution[1].people[ "4" ] );
+    sum += +( solution[1].people[ "5" ] );
+    sum += +( solution[2].people[ "4" ] );
+    sum += +( solution[2].people[ "5" ] );
+    sum += +( solution[3].people[ "3" ] );
 }
