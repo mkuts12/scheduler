21d6d0144b6d5d67f4fcb027daa28d782689f5d3
Michael Kuts <mkuts12@gmail.com>
2016-02-20 01:49:46 +0200

    changes due to publication

--- README.md
+++ README.md
@@ -1,13 +1,4 @@
-```person: {
-    id,
-    iluzim: [],
-}
+A simple project using 'genetic' library.
+This project will output the best schedule, based on a threshold, with respect to plugins you can write.
 
-shift: {
-    people: [],
-    iluzim,
-}
-
-iluz: {
-    
-}
+It is still a first draft but it works on simple case.
--- src/dataStore.js
+++ src/dataStore.js
@@ -2,48 +2,39 @@ module.exports = {
     people: [
         {
             id: 1,
-            ptorim: []
         },
         {
             id: 2,
-            ptorim: []
         },
         {
             id: 3,
-            ptorim: []
         },
         {
             id: 4,
-            ptorim: []
         },
         {
             id: 5,
-            ptorim: []
         }
     ],
     shifts: [
         {
             id: 1,
             peopleNeeded: 3,
-            ptorim: [],
             people: {},
         },
         {
             id: 2,
             peopleNeeded: 5,
-            ptorim: [],
             people: {},
         },
         {
             id: 3,
             peopleNeeded: 2,
-            ptorim: [],
             people: {},
         },
         {
             id: 4,
             peopleNeeded: 1,
-            ptorim: [],
             people: {},
         },
 
