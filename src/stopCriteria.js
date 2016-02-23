497001d253853dd511497668c90c6cb9186189e1
Michael Kuts <mkuts12@gmail.com>
2016-02-23 21:41:12 +0200

    trying to do a stagnation indicator

--- src/stopCriteria.js
+++ src/stopCriteria.js
@@ -1,5 +1,35 @@
 import { threshold } from '../options.js';
 
-export default function stopCriterion (){
-    return this.statistics ? this.statistics.maxScore >= threshold : false;
+// export default function stopCriterion (){
+//     return this.statistics ? this.statistics.maxScore >= threshold : false;
+// }
+
+function addStagnationIndictator(){
+    if( this.stagnation === undefined )
+        return;
+    this.stagnation = 0;
+    this.previousMaxScore = 0;
+}
+
+function getDiff(){
+    return this.statistics.maxScore - this.previousMaxScore;
+}
+
+function updateMaxScore(){
+    this.previousMaxScore = this.statistics.maxScore;
+    return false;
+}
+
+function isStagnated(){
+    this.stagnation = getDiff.call(this) <= threshold ? this.stagnating + 1 : 0;
+    return this.stagnation >= 5;
+}
+
+export default function stagnating (){
+    if( ! this.statistics )
+        return false;
+    addStagnationIndictator.call(this);
+    console.log("generation: " + this.generation);
+    debugger;
+    return isStagnated.call(this);
 }
